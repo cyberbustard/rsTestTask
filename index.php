656171559fdb2e1383c37aac83423d81331f074c
Artem <artem.y@zina.com.ua>
2020-11-07 01:51:19 +0200

    -header -slider -cards

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/slider.css">
     <script
             src="https://code.jquery.com/jquery-3.5.1.min.js"
             integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
@@ -15,10 +17,232 @@
 </head>
 <body>
     <header class="header">
+        <div class="container">
+        <div class="menu d-flex flex-column flex-md-row align-items-center p-3 px-md-4  bg-white  ">
+            <nav class="my-0 mr-md-auto">
+                <img class="arrow mb-1" src="img/arrow-rigth.png">
+                <a class="p-2 text-dark" href="#">О нас</a>
+                <img class="arrow mb-1" src="img/arrow-rigth.png">
+                <a class="p-2 text-dark" href="#">Новые поступления</a>
+                <img class="arrow mb-1" src="img/arrow-rigth.png">
+                <a class="p-2 text-dark" href="#">Доставка и оплата</a>
+                <img class="arrow mb-1" src="img/arrow-rigth.png">
+                <a class="p-2 text-dark" href="#">Прайс-листы</a>
+                <img class="arrow mb-1" src="img/arrow-rigth.png">
+                <a class="p-2 text-dark" href="#">Сотрудничество</a>
+                <img class="arrow mb-1" src="img/arrow-rigth.png">
+                <a class="p-2 text-dark" href="#">Контакты</a>
+            </nav>
+            <a class="" href="#">Вход/Регистрация</a>
 
 
+        </div>
     </header>
+    <section class="sub__header">
+        <div class="container">
+           <div class="sec__header">
 
+               <div class="row ">
+                   <div class="col-6">
+                       <div class="sub_header mt-2 d-flex">
+                           <img class="logo" src="img/logo.png" alt="Radio Store">
+                           <div class="arrow-v vertical-arrow"></div>
+                           <p class="pl-2 sub__header__desc">Электроные компоненты и<br>
+                               радиодетали, источники<br>
+                               питания и LED продукция</p>
+                       </div>
+                   </div>
+                   <div class="col d-inline-block">
+                                <div class="work-time pt-2 h-1 ">
+                                    <p class="work__time mb-0" ><img   class="sub__header-clock" src="img/clock.png" alt="clock">Пн -Пт  9:30 по 18:30</p>
+                                </div>
+
+                                <p class="contact__phone mb-0">096 597 68 62</p>
+
+                                <div class="phone-numbers d-flex">
+                                    <p class="more__phone-numbers ">Все телефоны<img class="arrow-down" src="img/arrow-rigth.png"></p>
+                                </div>
+                   </div>
+                   <div class="col">
+                       <div class=" adres-desc">
+                           <p><img class="location-img" src="img/location.png" alt="Our Location">Наши адреса<img class="arrow-down" src="img/arrow-rigth.png"></p>
+                           <p><img class="callback-icon" src="img/phone2.png" alt="get callback">Обратный звонок</p>
+                       </div>
+                   </div>
+                   <div class="col cart-block">
+                       <div class="img-wrap">
+                           <img class="cart float-left  "  src="img/cart.png" alt="Cart">
+                           <p class="productsCount float-left">0</p>
+                           <p class="productsPrice pl-5">Корзина <br <p class="totalPrice pl-5">0 грн</p> </p>
+                       </div>
+
+                   </div>
+
+               </div>
+    </section>
+    <div class="split-line"></div>
+    <section class="burger__menu">
+        <div class="container">
+            <div class="catalog d-flex">
+                <div class="burger-menu__inner d-flex">
+                    <div class="burger">
+                        <div class="tab-1"></div>
+                        <div class="tab-2"></div>
+                        <div class="tab-3"></div>
+                    </div>
+                    <div class="tab-4"></div>
+                    <p class="catalog-text" > <nobr>КАТАЛОГ ТОВАРОВ</nobr> </p>
+
+                </div>
+                <input class="input" type="textfield" name="search_products" placeholder="Поиск в каталоге" >
+            </div>
+        </div>
+           </div>
+        </div>
+    </section>
+
+
+    </div>
+
+    <section class="slider__section">
+        <div class="background">
+        <div class="container">
+            <div class="w3-content w3-display-container">
+
+                <div class="w3-display-container mySlides">
+                    <img class="slider-img" src="img/rope.png" style="width:35%">
+                    <div class="w3-display-bottomright w3-large w3-container w3-padding-16 w3-black">
+                        <div class="first__block">
+                            <p class="date">28 ноября - 29 декабря</p>
+                            <p class="sale">10%</p>
+                            <p class="sale-text" >Специальное предложение только<br>
+                                для вас -10% скидки </p>
+                        </div>
+
+                        <div class="second__block">
+                            <p class="product-description">Акустический кабель Dialan <br>
+                                медь 2*0,753мм прозрачный <br>
+                                ПВХ 1м </p>
+
+                            <p class="before-price">
+                                БЫЛО 13,60 грн.
+                            </p>
+                            <p class="after-price">
+                                12,24грн.
+                            </p>
+                            <button class="btn rounded buy-btn">УСПЕЙТЕ КУПИТЬ</button>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="w3-display-container mySlides">
+                    <img class="slider-img" src="img/rope.png" style="width:35%">
+                    <div class="w3-display-topleft w3-large w3-container w3-padding-16 w3-black">
+                        <div class="w3-display-bottomright w3-large w3-container w3-padding-16 w3-black">
+                            <div class="first__block">
+                                <p class="date">28 ноября - 29 декабря</p>
+                                <p class="sale">10%</p>
+                                <p class="sale-text" >Специальное предложение только<br>
+                                    для вас -10% скидки </p>
+                            </div>
+
+                            <div class="second__block">
+                                <p class="product-description">Акустический кабель Dialan <br>
+                                    медь 2*0,753мм прозрачный <br>
+                                    ПВХ 1м </p>
+
+                                <p class="before-price">
+                                    БЫЛО 13,60 грн.
+                                </p>
+                                <p class="after-price">
+                                    12,24грн.
+                                </p>
+                                <button class="btn rounded buy-btn">УСПЕЙТЕ КУПИТЬ</button>
+
+                            </div>
+                    </div>
+                </div>
+
+                <div class="w3-display-container mySlides">
+                    <img class="slider-img" src="img/rope.png" style="width:35%">
+                    <div class="w3-display-topright w3-large w3-container w3-padding-16 w3-black">
+                        <div class="w3-display-bottomright w3-large w3-container w3-padding-16 w3-black">
+                            <div class="first__block">
+                                <p class="date">28 ноября - 29 декабря</p>
+                                <p class="sale">10%</p>
+                                <p class="sale-text" >Специальное предложение только<br>
+                                    для вас -10% скидки </p>
+                            </div>
+
+                            <div class="second__block">
+                                <p class="product-description">Акустический кабель Dialan <br>
+                                    медь 2*0,753мм прозрачный <br>
+                                    ПВХ 1м </p>
+
+                                <p class="before-price">
+                                    БЫЛО 13,60 грн.
+                                </p>
+                                <p class="after-price">
+                                    12,24грн.
+                                </p>
+                                <button class="btn rounded  buy-btn">УСПЕЙТЕ КУПИТЬ</button>
+
+                            </div>
+                    </div>
+                </div>
+
+
+                <button class="w3-button w3-display-left w3-black" onclick="plusDivs(-1)">&#10094;</button>
+                <button class="w3-button w3-display-right w3-black" onclick="plusDivs(1)">&#10095;</button>
+            </div>
+    </section>
+
+    <section class="products-type ">
+        <div class="container">
+            <div class="card-deck mb-3 text-center ">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-body-1">
+                        <h1 class="card-title pricing-card-title"> АКЦИОННЫЕ ТОВАРЫ</h1>
+                        <div class="horizontal-line-1"></div>
+                        <p class="card-content">Поступление нового товара за <br>
+                            последние 7 календарных дней. <br>
+                            Лучшие цены в Украине</p>
+                        <button type="button" class="btn  rounded btn-details-1">ПОДРОБНЕЕ</button>
+                    </div>
+                </div>
+                <div class="card mb-4 shadow-sm">
+
+                    <div class="card-body-2">
+                        <h1 class="card-title pricing-card-title">КАТАЛОГ ТОВАРОВ</h1>
+                        <div class="horizontal-line-2"></div>
+                        <p class="card-content">Поступление нового товара за <br>
+                            последние 7 календарных дней. <br>
+                            Лучшие цены в Украине</p>
+                        <button type="button" class="btn rounded btn-details-2">ПОДРОБНЕЕ</button>
+                    </div>
+                </div>
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-body-3">
+                        <h1 class="card-title pricing-card-title">НОВЫЕ ПОСТУПЛЕНИЯ</h1>
+                        <div class="horizontal-line-3"></div>
+                        <p class="card-content">Поступление нового товара за <br>
+                            последние 7 календарных дней. <br>
+                            Лучшие цены в Украине</p>
+                        <button type="button" class="btn rounded  btn-details-3">ПОДРОБНЕЕ</button>
+                    </div>
+                </div>
+            </div>
+
+    </section>
+
+
+
+
+
+
+    <script src="js/slider.js"></script>
     <script src="js/main.js"></script>
 </body>
 
