0c2da37e1b052c1dfebedc1491e0d241cbe39ddf
Artem <artem.y@zina.com.ua>
2020-11-06 12:37:22 +0200

    f commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
+    <script
+            src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous"></script>
+
+    <title>Radio Store</title>
+</head>
+<body>
+    <header class="header">
+
+
+    </header>
+
+    <script src="js/main.js"></script>
+</body>
+
+
+</html>
